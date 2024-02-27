90b508361bc4bc693de6bc0278c88553dd750d2a
Derek Stengel <derek.stengel@gmail.com>
2024-02-27 11:38:08 -0700

    added personality quiz

---
+++ Personality Quiz/Personality Quiz/IntroductionViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Personality Quiz
+//
+//  Created by Derek Stengel on 2/26/24.
+//
+
+import UIKit
+
+class IntroductionViewController: UIViewController {
+    
+    @IBAction func unwindToQuizIntroduction(segue: UIStoryboardSegue) {
+        
+    }
+    
+    
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
---
+++ Personality Quiz/Personality Quiz/ResultsViewController.swift
@@ -0,0 +1,60 @@
+//
+//  ResultsViewController.swift
+//  Personality Quiz
+//
+//  Created by Derek Stengel on 2/26/24.
+//
+
+import UIKit
+
+class ResultsViewController: UIViewController {
+    
+    @IBOutlet weak var resultAnswerLabel: UILabel!
+    @IBOutlet weak var resultDefinitionLabel: UILabel!
+    
+    
+
+    var responses: [Answer]
+    
+    init?(coder: NSCoder, responses: [Answer]) {
+        self.responses = responses
+        super.init(coder: coder)
+    }
+    
+    required init?(coder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        navigationItem.hidesBackButton = true
+        calculatePersonalityResult()
+    }
+    
+    func calculatePersonalityResult() {
+        let frequencyOfAnswers = responses.reduce(into: [AnimalType:Int]()) {
+            (counts, answer) in
+            
+            counts[answer.type, default: 0] += 1
+            
+            
+        }
+        
+        let frequentAnswersSorted = frequencyOfAnswers.sorted(by:
+        { (pair1, pair2) in
+            return pair1.value > pair2.value
+        })
+        let mostCommonAnswer = frequencyOfAnswers.sorted {$0.1 > $1.1 }.first!.key
+        
+        resultAnswerLabel.text = "You are a \(mostCommonAnswer.rawValue)!"
+        resultDefinitionLabel.text = mostCommonAnswer.definition
+        
+        
+    }
+    
+    
+    
+    
+                                                            
+    
+}
